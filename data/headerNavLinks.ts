486939b47a059dd008bd9524b5fe5a27c5a6c2e0
anish-lakkapragada <anish.lakkapragada@gmail.com>
2024-09-21 22:42:11 -0400

    .mdx files.

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/gallery', title: 'Gallery' },
   { href: '/about', title: 'About' },
 ]
 
